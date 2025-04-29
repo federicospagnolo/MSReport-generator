1cc54e88dc17f7d739c7fdcb4ef9c0aac6e01473
Federico Spagnolo <federico.spagnolo@unibas.ch>
2025-04-29 10:18:05 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -59,7 +59,7 @@ def form_cluster(data_array:np.array, struct:np.array=np.ones([3, 3, 3])):
 # current folder
 script_folder = os.path.dirname(os.path.realpath(__file__))
 # set output folder
-output_dir = "SYNTHSEG"
+output_dir = "SAMSEG"
 
 parser = argparse.ArgumentParser(
 description='''Saliency map generation.

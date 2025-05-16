1cddd1c0494a771d00330a863bb36a60f59aa261
Federico Spagnolo <federico.spagnolo@unibas.ch>
2025-05-16 05:55:51 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -99,9 +99,8 @@ for layer in model.model.modules():
 # Load best model weights
 model_path = dp(script_folder, model_checkpoint)
 #print(model_path)
-#model = torch.nn.DataParallel(model).cuda()
-#model.load_state_dict(torch.load(args.model_checkpoint, map_location='cuda'))
-model.load_state_dict(torch.load(args.model_checkpoint))
+model = torch.nn.DataParallel(model).cuda()
+model.load_state_dict(torch.load(args.model_checkpoint, map_location='cuda'))
 model.eval()
 activation = torch.nn.Softmax(dim=1)
 

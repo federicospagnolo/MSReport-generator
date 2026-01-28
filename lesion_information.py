bf463e62536418100f8d4bf76bc4df4b908501e4
Federico Spagnolo <federico.spagnolo@unibas.ch>
2026-01-28 14:30:35 +0100

    Refactor lesion segmentation logic
    Removed binary dilation for white matter segmentation and updated lesion type detection.

--- lesion_information.py
+++ lesion_information.py
@@ -110,7 +110,6 @@ for image_name, mask_name in zip(image_name_list, mask_name_list):
     seg_cortex = ndimage.binary_dilation(seg_cortex_undil, structure=struct1, iterations=1)
     seg_infratentorial = ndimage.binary_dilation(seg_infratentorial_undil, structure=struct1, iterations=1).astype(int)
     seg_ventricles = ndimage.binary_dilation(seg_ventricles_undil, structure=struct1, iterations=1).astype(int)
-    seg_wm = ndimage.binary_dilation(seg_wm_undil, structure=struct1, iterations=2).astype(int)
     
     #if save_labelmap:
     #    mask_file_name = os.path.basename(mask_name)
@@ -133,7 +132,7 @@ for image_name, mask_name in zip(image_name_list, mask_name_list):
         cortex = bool(np.sum(lesion_seg & seg_cortex))
         infratentorial = bool(np.sum(lesion_seg & seg_infratentorial))
         periventricular = bool(np.sum(lesion_seg & seg_ventricles))
-        wm = bool(np.sum(lesion_seg & seg_wm))      
+    
         if infratentorial:
             lesion_type = 'infratentorial'
         elif periventricular:
